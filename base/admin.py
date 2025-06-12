16a05677544ed0f80297d9c31ae47ca87ed4f23a
Tarun <tarunsaisatish@gmail.com>
2025-06-12 00:11:28 -0700

    Delete base directory

--- base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import studentdetails
-from .models import DonorDetails
-
-admin.site.register(studentdetails)
-admin.site.register(DonorDetails)
