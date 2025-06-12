7c8aafd60f4efb3c45f4224c49397491572dcd7c
G TARUN <gtarun@Gs-MacBook-Air.local>
2025-06-12 13:00:47 -0700

    Use environment variable for SECRET_KEY

--- LifeDrop/settings.py
+++ LifeDrop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xz$il-=lg@8zod!93!ye*mxr9xujk)h8ao1d9^c6wlo12i1_#q'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret-key-for-dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
